1e90f481cc1cc70189e7e2a57855e30d5f3785e3
Doan Quoc Cuong <cuong.dq@mobifone.vn>
2025-09-15 19:30:29 +0700

    fix publish

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,46 @@
-import typescript from '@rollup/plugin-typescript';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import esbuild from 'rollup-plugin-esbuild'
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import esbuild from "rollup-plugin-esbuild";
 
 export default {
-  input: 'src/index.ts', // Điểm vào từ mã TypeScript. -- phương án build trong tham số lệnh
+  input: "src/index.ts", // Điểm vào từ mã TypeScript. -- phương án build trong tham số lệnh
   external: [], // Không có external deps
   output: [
     {
-      file: 'lib/index.js', // CommonJS use: import ... from '...';
-      format: 'cjs',
+      file: "lib/index.js", // CommonJS use: import ... from '...';
+      format: "cjs",
       sourcemap: true, // Tùy chọn: Thêm source map để debug
-      exports: 'auto'
+      exports: "auto",
     },
     {
-      file: 'lib/index.mjs', // ES Module: use = require('..');
-      format: 'esm',
+      file: "lib/index.mjs", // ES Module: use = require('..');
+      format: "esm",
       sourcemap: true,
     },
     {
-      file: 'lib/index.umd.js', // UMD cho browser và hybrid
-      format: 'umd',
-      name: 'UniversalMongodb', // Tên global variable cho browser
+      file: "lib/index.umd.js", // UMD cho browser và hybrid
+      format: "umd",
+      name: "DQCAIMongoDB", // Tên global variable cho browser
       sourcemap: true,
     },
   ],
   plugins: [
+    json(), // Thêm plugin này
     nodeResolve(), // Giải quyết các phụ thuộc node_modules
     commonjs(), // Chuyển đổi CommonJS sang ESM nếu cần
     typescript({
-      tsconfig: './tsconfig.json', declaration: true,
-      declarationDir: 'lib',
+      tsconfig: "./tsconfig.json",
+      declaration: true,
+      declarationDir: "lib",
       declarationMap: true, // Tạo .d.ts.map để debug
-      rootDir: 'src'
+      rootDir: "src",
     }), // Biên dịch TypeScript
     esbuild({
       minify: true,
-      target: 'es2017' // Target ES version phù hợp: ES2017+ cho modern browsers // RN 0.60+
-    })
-
+      target: "es2017", // Target ES version phù hợp: ES2017+ cho modern browsers // RN 0.60+
+    }),
   ],
-  external: [], // Chỉ định các phụ thuộc bên ngoài nếu cần (ví dụ: không bundle tslib nếu dùng importHelpers)
+  external: ['mongodb'] // MongoDB nên là external dependency
 };
